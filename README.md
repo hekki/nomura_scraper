5a278316ad2044e506889eae3feb2215f22d5956
Hiroki Ito <contact@hekki.info>
2019-08-29 01:29:42 +0900

    init

---
+++ README.md
@@ -0,0 +1,13 @@
+# nomura_scraper
+- 野村證券の投資信託用ファンド詳細ページ (ex. [eMAXIS NYダウインデックス](https://advance.quote.nomura.co.jp/meigara/nomura2/qsearch.exe?F=users/nomura/detail2&KEY1=03311138)) をスクレイピングしてslack に通知します
+- nomura_scraper は野村證券株式会社と一切関係がない、非公式なプロジェクトです
+
+## Install
+### for macOS
+- `brew tap homebrew/cask && brew cask install phantomjs`
+    - cf. https://github.com/teampoltergeist/poltergeist#mac
+- `bundle install -j4 --path vendor/bundle`
+
+## 免責事項
+- 当ツールのご利用は利用者の責任において行ってください
+- 当ツールの開発者は、当プロジェクトの利用により生じた損害に対する責任を負いません
---
+++ nomura_scraper.rb
@@ -0,0 +1,30 @@
+require 'bundler'
+Bundler.require
+
+require './web_client'
+require './fund'
+require './slack_notifier'
+require './slack_attachment'
+
+# NOTE: 野村證券の投資信託ファンド詳細ページのURLに含まれる 'KEY1' を、カンマ区切りで環境変数 'nomura_keys' に設定する
+#   URL example: https://advance.quote.nomura.co.jp/meigara/nomura2/qsearch.exe?F=users/nomura/detail2&KEY1=99999999
+def keys
+  keys = ENV['nomura_keys']
+  return [] if keys.nil?
+
+  keys.split(',').map(&:strip)
+end
+
+def slack_channel
+  ENV['SLACK_CHANNEL'].nil? ? '#general' : ENV['SLACK_CHANNEL']
+end
+
+keys.each do |key|
+  # web_client = WebClient.new '03311138'
+  web_client = WebClient.new key
+  fund = Fund.new web_client.body
+
+  slack_notifier = SlackNotifier.new
+  slack_attachemnt = SlackAttachment.new fund: fund, web_client: web_client
+  slack_notifier.post channel: slack_channel, attachments: slack_attachemnt.make
+end
